90418f52d97937c6eaf0a6b9bafed83b19077b16
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-24 13:53:44 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,57 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+
+
+
+
+	public static int sum2D(int[][] array2D)
+	{
+		int sum = 0;
+		for (int row = 0 ; row < array2D.length; row++)
+		{
+			for (int column = 0; column < array2D[row].length; column++)
+			{
+				sum += array2D[row][column];
+			}
+		}
+		return sum;
+	}
+
+
+
+
+	public static int sumRow(int[][] array2D , int rowNum )
+	{
+		int sum = 0;
+		for (int column = 0; column < array2D[rowNum].length; column++)
+		{
+			sum += array2D[rowNum][column];
+		}
+		return sum;
+	}
+
+
+
+
+
+
+	public static int sumColumn(int[][] array2D , int column )
+	{
+		int sum = 0;
+		for (int row = 0; row < array2D.length; row++)
+		{
+			sum += array2D[row][column];
+		}
+		return sum;
+	}
+
+
+
+
+
+
+	
+
+
 }
